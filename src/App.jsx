79710f49e3bc35e5a5c051abdcdd2cdad9ffbb8c
Trudy Wong <trudypwong@gmail.com>
2025-04-12 09:25:35 -0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import { useState } from 'react';
+import './App.css';
+
+function App() {
+  const [colour, setColour] = useState('');
+
+  return (
+    <>
+      <h1>Docker Assignment #2</h1>
+      <p>SSD3900 DevOps and Containerization</p>
+      <div className="container" style={{ borderColor: colour }}>
+        <p>Trying out input type='color'</p>
+        <div className="row">
+          <label for="colour">Colour</label>
+          <input
+            id="colour"
+            name="colour"
+            type="color"
+            onChange={(e) => setColour(e.target.value)}
+            value={colour}
+          />
+          <p style={{ color: colour }}>{colour}</p>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
